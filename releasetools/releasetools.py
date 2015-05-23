be0d9233c400e52df9123e53319e78f72a1707eb
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-05-23 09:40:50 +0200

    OTA script: Fix duplicated system mount

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -24,12 +24,22 @@
 # Do not shorten the search or you may change the actual kernel source
 #
 
-"""Custom OTA commands for LG devices with locked bootloaders"""
+"""Custom OTA commands for LG devices"""
+
+import os
+import re
+
+FINAL_PRODUCT = os.getenv('TARGET_PRODUCT')
+CM_VERSION = os.getenv('CM_VERSION')
 
 def FullOTA_InstallEnd(info):
   info.script.script = [cmd for cmd in info.script.script if not "boot.img" in cmd]
   info.script.script = [cmd for cmd in info.script.script if not "show_progress(0.100000, 0);" in cmd]
   info.script.AppendExtra('package_extract_file("boot.img", "/tmp/boot.img");')
+  
+  if re.match('^cm_', FINAL_PRODUCT) and re.match('^12', CM_VERSION):
+    info.script.Unmount("/system")
+  
   info.script.Mount("/system")
   info.script.AppendExtra('assert(run_program("/system/bin/panel.sh") == 0);')
   info.script.AppendExtra('assert(run_program("/system/bin/loki.sh") == 0);')
