dd6ea0be01e12b65d8b06ac2a26411714f751320
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-06-15 15:48:19 +0200

    Revert "Update GPS HAL (1/2)"
    This reverts commit ae4423ee095aefe4dcbf40e1ea43456bed9b2cb0.

--- g2.mk
+++ g2.mk
@@ -177,15 +177,11 @@ PRODUCT_BOOT_JARS += qcmediaplayer
 
 # GPS
 PRODUCT_PACKAGES += \
-    gps.msm8974
-
-# GPS configuration
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+    libloc_adapter \
+    libloc_eng \
+    libloc_api_v02 \
+    libgps.utils \
+    gps.g2
 
 PRODUCT_PACKAGES += \
     hwaddrs
